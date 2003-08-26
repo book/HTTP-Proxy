75a23d850140b42a5ad5d8e9b7890c0466097760
Philippe "BooK" Bruhat <book@cpan.org>
2003-08-26 20:55:21 +0000

    Add Cookie2 to the list of no-no headers

--- eg/anonymiser.pl
+++ eg/anonymiser.pl
@@ -12,10 +12,10 @@ $proxy->logmask( shift || NONE );
 $proxy->push_filter(
     mime    => undef,
     request => HTTP::Proxy::HeaderFilter::simple->new(
-        sub { $_[1]->remove_header(qw( User-Agent From Referer Cookie )) }
+        sub { $_[1]->remove_header(qw( User-Agent From Referer Cookie Cookie2 )) }
     ),
     response => HTTP::Proxy::HeaderFilter::simple->new(
-        sub { $_[1]->remove_header(qw( Set-Cookie )) }
+        sub { $_[1]->remove_header(qw( Set-Cookie Set-Cookie2 )) }
     )
 );
 
