810385554927fcc136e5b65facaf2ef996f46de5
Philippe "BooK" Bruhat <book@cpan.org>
2004-01-05 23:00:43 +0000

    Yet another example filter. Useful for writing bots.

---
+++ eg/logger.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl -w
+use strict;
+use HTTP::Proxy qw( :log );
+use HTTP::Proxy::HeaderFilter::simple;
+use HTTP::Proxy::BodyFilter::simple;
+use CGI::Util qw( unescape );
+
+# NOTE: Body request filters always receive the request body in one pass
+my $post_filter = HTTP::Proxy::BodyFilter::simple->new(
+    sub {
+        my ( $self, $dataref, $message, $protocol, $buffer ) = @_;
+        print STDOUT $message->method, " ", $message->uri, "\n";
+        print_headers( $message, qw( Cookie Cookie2 ));
+
+        # this is from CGI.pm, method parse_params
+        my (@pairs) = split ( /[&;]/, $$dataref );
+        for (@pairs) {
+            my ( $param, $value ) = split ( '=', $_, 2 );
+            $param = unescape($param);
+            $value = unescape($value);
+            printf STDOUT "    %-30s => %s\n", $param, $value;
+        }
+    }
+);
+
+my $get_filter = HTTP::Proxy::HeaderFilter::simple->new(
+    sub {
+        my ( $self, $headers, $message ) = @_;
+        my $req = $message->request;
+        if( $req->method ne 'POST' ) {
+            print STDOUT $req->method, " ", $req->uri, "\n";
+            print_headers( $req, qw( Cookie Cookie2 ));
+        }
+        print_headers( $message, qw( Content-Type Set-Cookie Set-Cookie2 ));
+    }
+);
+
+sub print_headers {
+    my $message = shift;
+    for my $h (@_) {
+        if( $message->header($h) ) {
+            print STDOUT "    $h: $_\n" for ( $message->header($h) );
+        }
+    }
+}
+
+my $proxy = HTTP::Proxy->new;
+$proxy->logmask( shift || NONE );
+$proxy->push_filter( method => 'POST', request => $post_filter );
+$proxy->push_filter( response => $get_filter );
+$proxy->start;
+
