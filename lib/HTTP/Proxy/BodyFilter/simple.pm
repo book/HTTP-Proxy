6ba5c9e69dfdb2f902fbda77c1ad9f77cbd862b7
Philippe "BooK" Bruhat <book@cpan.org>
2005-03-29 14:45:34 +0000

    Better pod coverage

--- lib/HTTP/Proxy/BodyFilter/simple.pm
+++ lib/HTTP/Proxy/BodyFilter/simple.pm
@@ -6,57 +6,6 @@ use HTTP::Proxy::BodyFilter;
 use vars qw( @ISA );
 @ISA = qw( HTTP::Proxy::BodyFilter );
 
-=head1 NAME
-
-HTTP::Proxy::BodyFilter::simple - A class for creating simple filters
-
-=head1 SYNOPSIS
-
-    use HTTP::Proxy::BodyFilter::simple;
-
-    # a simple s/// filter
-    my $filter = HTTP::Proxy::BodyFilter::simple->new(
-        sub { ${ $_[0] } =~ s/foo/bar/g; }
-    );
-    $proxy->push_filter( response => $filter );
-
-=head1 DESCRIPTION
-
-HTTP::Proxy::BodyFilter::simple can create BodyFilter without going
-through the hassle of creating a full-fledged class. Simply pass
-a code reference to the filter() method of your filter to the constructor,
-and you'll get the adequate filter.
-
-=head2 Constructor calling convention
-
-The constructor can be called in several ways, which are shown in the
-synopsis:
-
-=over 4
-
-=item single code reference
-
-The code reference must conform to the standard filter() signature:
-
-    sub filter {
-        my ( $self, $dataref, $message, $protocol, $buffer ) = @_;
-        ...
-    }
-
-It is assumed to be the code for the filter() method.
-See HTTP::Proxy::BodyFilter.pm for more details about the filter() method.
-
-=item name/coderef pairs
-
-The name is the name of the method (C<filter>, C<begin>, C<end>)
-and the coderef is the method itself.
-
-See HTTP::Proxy::BodyFilter for the methods signatures.
-
-=back
-
-=cut
-
 my $methods = join '|', qw( begin start filter end );
 $methods = qr/^(?:$methods)$/;
 
@@ -97,6 +46,97 @@ sub can {
       : UNIVERSAL::can( $self, $method );
 }
 
+1;
+
+__END__
+
+=head1 NAME
+
+HTTP::Proxy::BodyFilter::simple - A class for creating simple filters
+
+=head1 SYNOPSIS
+
+    use HTTP::Proxy::BodyFilter::simple;
+
+    # a simple s/// filter
+    my $filter = HTTP::Proxy::BodyFilter::simple->new(
+        sub { ${ $_[0] } =~ s/foo/bar/g; }
+    );
+    $proxy->push_filter( response => $filter );
+
+=head1 DESCRIPTION
+
+HTTP::Proxy::BodyFilter::simple can create BodyFilter without going
+through the hassle of creating a full-fledged class. Simply pass
+a code reference to the C<filter()> method of your filter to the constructor,
+and you'll get the adequate filter.
+
+=head2 Constructor calling convention
+
+The constructor can be called in several ways, which are shown in the
+synopsis:
+
+=over 4
+
+=item single code reference
+
+The code reference must conform to the standard filter() signature:
+
+    sub filter {
+        my ( $self, $dataref, $message, $protocol, $buffer ) = @_;
+        ...
+    }
+
+It is assumed to be the code for the C<filter()> method.
+See HTTP::Proxy::BodyFilter.pm for more details about the C<filter()> method.
+
+=item name/coderef pairs
+
+The name is the name of the method (C<filter>, C<begin>, C<end>)
+and the coderef is the method itself.
+
+See HTTP::Proxy::BodyFilter for the methods signatures.
+
+=back
+
+=head1 METHODS
+
+This filter "factory" defines the standard HTTP::Proxy::BodyFilter
+methods, but those are only, erm, "proxies" to the actual CODE references
+passed to the constructor. These "proxy" methods are:
+
+=over 4
+
+=item filter()
+
+=item begin()
+
+=item end()
+
+=back
+
+Two other methods are actually HTTP::Proxy::BodyFilter::simple methods,
+and are called automatically:
+
+=over 4
+
+=item init()
+
+Initalise the filter instance with the code references passed to the
+constructor.
+
+=item can()
+
+Return the actual code reference that will be run, and not the "proxy"
+methods. If called with any other name than C<begin>, C<end> and
+C<filter>, calls UNIVERSAL::can() instead.
+
+=back
+
+=head1 SEE ALSO
+
+L<HTTP::Proxy>, L<HTTP::Proxy::BodyFilter>.
+
 =head1 AUTHOR
 
 Philippe "BooK" Bruhat, E<lt>book@cpan.orgE<gt>.
@@ -112,4 +152,3 @@ the same terms as Perl itself.
 
 =cut
 
-1;
