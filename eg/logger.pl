68e3218fdcf6a84440fe0bb41770286f4440cd11
Philippe "BooK" Bruhat <book@cpan.org>
2005-06-18 22:07:25 +0000

    A cleaner version of logger.pl, which allows it to peek selectively at some sites.

--- eg/logger.pl
+++ eg/logger.pl
@@ -1,12 +1,27 @@
 #!/usr/bin/perl -w
 use strict;
-use HTTP::Proxy qw( :log );
+use HTTP::Proxy;
 use HTTP::Proxy::HeaderFilter::simple;
 use HTTP::Proxy::BodyFilter::simple;
 use CGI::Util qw( unescape );
 
+# get the command-line parameters
+my @peek;
+{
+    my $i = 0;
+    while ( $i < @ARGV ) {
+        if ( $ARGV[$i] eq 'peek' ) {
+            push @peek, $ARGV[ $i + 1 ];
+            splice( @ARGV, $i, 2 );
+            next;
+        }
+        $i += 2;
+    }
+}
+
+# the headers we want to see
 my @srv_hdr = qw( Content-Type Set-Cookie Set-Cookie2 WWW-Authenticate
-                  Location );
+  Location );
 my @clt_hdr = qw( Cookie Cookie2 Referer Referrer Authorization );
 
 # NOTE: Body request filters always receive the request body in one pass
@@ -14,12 +29,12 @@ my $post_filter = HTTP::Proxy::BodyFilter::simple->new(
     sub {
         my ( $self, $dataref, $message, $protocol, $buffer ) = @_;
         print STDOUT $message->method, " ", $message->uri, "\n";
-        print_headers( $message, @clt_hdr);
+        print_headers( $message, @clt_hdr );
 
         # this is from CGI.pm, method parse_params
-        my (@pairs) = split ( /[&;]/, $$dataref );
+        my (@pairs) = split( /[&;]/, $$dataref );
         for (@pairs) {
-            my ( $param, $value ) = split ( '=', $_, 2 );
+            my ( $param, $value ) = split( '=', $_, 2 );
             $param = unescape($param);
             $value = unescape($value);
             printf STDOUT "    %-30s => %s\n", $param, $value;
@@ -31,9 +46,9 @@ my $get_filter = HTTP::Proxy::HeaderFilter::simple->new(
     sub {
         my ( $self, $headers, $message ) = @_;
         my $req = $message->request;
-        if( $req->method ne 'POST' ) {
+        if ( $req->method ne 'POST' ) {
             print STDOUT $req->method, " ", $req->uri, "\n";
-            print_headers( $req, @clt_hdr);
+            print_headers( $req, @clt_hdr );
         }
         print STDOUT "    ", $message->status_line, "\n";
         print_headers( $message, @srv_hdr );
@@ -43,14 +58,21 @@ my $get_filter = HTTP::Proxy::HeaderFilter::simple->new(
 sub print_headers {
     my $message = shift;
     for my $h (@_) {
-        if( $message->header($h) ) {
+        if ( $message->header($h) ) {
             print STDOUT "    $h: $_\n" for ( $message->header($h) );
         }
     }
 }
 
-my $proxy = HTTP::Proxy->new( @ARGV );
-$proxy->push_filter( method => 'POST', request => $post_filter );
-$proxy->push_filter( response => $get_filter );
+# create and start the proxy
+my $proxy = HTTP::Proxy->new(@ARGV);
+for (@peek) {
+    $proxy->push_filter(
+        host    => $_,
+        method  => 'POST',
+        request => $post_filter
+    );
+    $proxy->push_filter( host => $_, response => $get_filter );
+}
 $proxy->start;
 
