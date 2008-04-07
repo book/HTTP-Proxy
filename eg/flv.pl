854abd3686853083abe530ec16308823aa071a0e
Philippe Bruhat (BooK) <book@cpan.org>
2008-04-07 09:35:27 +0000

    example script that saves Flash videos

---
+++ eg/flv.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use HTTP::Proxy;
+use HTTP::Proxy::BodyFilter::save;
+use Digest::MD5 qw( md5_hex);
+
+my $proxy = HTTP::Proxy->new(@ARGV);
+
+# a filter to save FLV files somewhere
+my $flv_filter = HTTP::Proxy::BodyFilter::save->new(
+    filename => sub {
+        my ($message) = @_;
+        my $uri = $message->request->uri;
+
+        # get the id, or fallback to some md5 hash
+        my ($id) = $uri->query =~ /id=([^&;]+)/i;
+        $id = md5_hex($uri) unless $id;
+
+        # compute the filename (including the base site name)
+        my ($host) = $uri->host =~ /([^.]+\.[^.]+)$/;
+        my $file = "flv/${host}_$id.flv";
+
+        # ignore it if we already have it
+        return if -e $file && -s $file == $message->content_length;
+
+        # otherwise, save
+        return $file;
+    },
+);
+
+# push the filter for all MIME types we want to catch
+for my $mime (qw( video/flv video/x-flv )) {
+    $proxy->push_filter(
+        mime     => $mime,
+        response => $flv_filter,
+    );
+}
+
+$proxy->start;
+
